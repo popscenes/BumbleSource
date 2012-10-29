e2e1a326479298ed54231c1b3b35d1bd20a3ff0d
firinflames <rickyaudsley@gmail.com>
2012-10-29 22:43:01 +1100

    fix up claim controller

---
+++ PostaFlya/PostaFlya.Specification/Fliers/Boards.feature
@@ -0,0 +1,6 @@
+﻿Feature: Boards
+	In order for a range of people to see my flier
+	As a participant
+	I want to be able to add my flier to a range of boards
+
+
--- PostaFlya/PostaFlya/Models/Claims/CreateClaimModel.cs
+++ PostaFlya/PostaFlya/Models/Claims/CreateClaimModel.cs
@@ -1,18 +1,23 @@
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using Website.Application.Extension.Validation;
 using PostaFlya.Binding;
 
 namespace PostaFlya.Models.Claims
 {
+    [DataContract]
     public class CreateClaimModel
     {
         [RequiredWithMessage]
+        [DataMember(IsRequired = true)]
         public EntityTypeEnum ClaimEntity { get; set; }
             
         [RequiredWithMessage]
+        [DataMember]
         public string EntityId { get; set; }
         
         [RequiredWithMessage]
+        [DataMember]
         public string BrowserId { get; set; }
     }
 }
