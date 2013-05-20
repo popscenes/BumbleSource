8af36649c2e01321e12ba8021d03ae25aab25092
firinflames <rickyaudsley@gmail.com>
2013-05-20 23:09:29 +1000

    more widget

---
+++ PostaFlya/PostaFlya/Scripts/Widget/WidgetLoad.js
@@ -0,0 +1,26 @@
+﻿(function () {
+
+    var scriptTag = document.createElement('script');
+    scriptTag.setAttribute("type", "text/javascript");
+    scriptTag.setAttribute("src", bf.widgetsrc);
+    if (scriptTag.readyState) {
+        scriptTag.onreadystatechange = function () { // For old versions of IE
+            if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                scriptLoadHandler();
+            }
+        };
+    } else { // Other browsers
+        scriptTag.onload = scriptLoadHandler;
+    }
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(scriptTag);
+
+    function scriptLoadHandler() {
+        // Restore $ and window.jQuery to their previous values and store the
+        // new jQuery in our local jQuery variable
+        var jQueryInt = window.jQuery.noConflict(true);
+
+        bf.widgetload(window, jQueryInt);
+ 
+    }
+
+})(); 
