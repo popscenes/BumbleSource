564d89df0f16ae248c7b670b316ff5334e6d8577
firinflames <rickyaudsley@gmail.com>
2013-05-30 01:29:50 +1000

    board search start

---
+++ PostaFlya/PostaFlya/Controllers/PagedModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace PostaFlya.Controllers
+{
+    [DataContract]
+    public class PagedModel
+    {
+        [DataMember(IsRequired = false)]
+        public int Count { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public int Skip { get; set; }
+    }
+}
