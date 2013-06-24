a7fad8c6d67b8583bf55b74c78de585e6d74e2ba
firinflames <rickyaudsley@gmail.com>
2013-06-24 22:21:18 +1000

    find venue board

---
+++ PostaFlya/PostaFlya.Domain/Flier/Query/GetFlyerVenueBoardQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostaFlya.Domain.Boards;
+using Website.Infrastructure.Query;
+
+namespace PostaFlya.Domain.Flier.Query
+{
+    public class GetFlyerVenueBoardQuery : QueryInterface
+    {
+        public FlierInterface Flyer { get; set; }
+    }
+
+    public class GetFlyerVenueBoardQueryHandler : QueryHandlerInterface<GetFlyerVenueBoardQuery, Board>
+    {
+        private readonly QueryChannelInterface _queryChannel;
+
+        public GetFlyerVenueBoardQueryHandler(QueryChannelInterface queryChannel)
+        {
+            _queryChannel = queryChannel;
+        }
+
+        public Board Query(GetFlyerVenueBoardQuery argument)
+        {
+            var boards = _queryChannel.Query(new FindByIdsQuery() {Ids = argument.Flyer.Boards.Select(b => b.BoardId)}, new List<Board>());
+            return boards.FirstOrDefault(board => board.Venue() != null);
+        }
+    }
+}
