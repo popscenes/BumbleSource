2fa7089ee50835c98040419e9c14e2a8fea29c76
firinflames <rickyaudsley@gmail.com>
2013-06-30 15:52:24 +1000

    more refactor

--- PostaFlya/PostaFlya.Domain/Flier/Query/FindFlyersByLatestQuery.cs
+++ PostaFlya/PostaFlya.Domain/Flier/Query/FindFlyersByLatestQuery.cs
@@ -1,4 +1,5 @@
-﻿using Website.Infrastructure.Query;
+﻿using System.Collections.Generic;
+using Website.Infrastructure.Query;
 
 namespace PostaFlya.Domain.Flier.Query
 {
@@ -7,5 +8,22 @@ namespace PostaFlya.Domain.Flier.Query
         public int Take { get; set; }
 
         public string Skip { get; set; }
+    }
+
+    public class FindFlyersByFeaturedQueryHandler : QueryHandlerInterface<FindFlyersByLatestQuery, List<Flier>>
+    {
+        private readonly QueryChannelInterface _queryChannel;
+
+        public FindFlyersByFeaturedQueryHandler(QueryChannelInterface queryChannel)
+        {
+            _queryChannel = queryChannel;
+        }
+
+        public List<Flier> Query(FindFlyersByLatestQuery argument)
+        {
+            var ret = _queryChannel.Query(argument, new List<string>());
+            return _queryChannel.Query(new FindByIdsQuery<Flier> { Ids = ret }, (List<Flier>)null);
+
+        }
     }
 }
--- PostaFlya/PostaFlya.Domain/Flier/Query/FindFlyersByLocationAndDistanceQuery.cs
+++ PostaFlya/PostaFlya.Domain/Flier/Query/FindFlyersByLocationAndDistanceQuery.cs
@@ -1,4 +1,5 @@
-﻿using Website.Domain.Location;
+﻿using System.Collections.Generic;
+using Website.Domain.Location;
 using Website.Infrastructure.Query;
 
 namespace PostaFlya.Domain.Flier.Query
@@ -12,5 +13,22 @@ namespace PostaFlya.Domain.Flier.Query
         public int Take { get; set; }
 
         public string Skip { get; set; }
+    }
+
+    public class FindFlyersByLocationAndDistanceQueryHandler : QueryHandlerInterface<FindFlyersByLocationAndDistanceQuery, List<Flier>>
+    {
+        private readonly QueryChannelInterface _queryChannel;
+
+        public FindFlyersByLocationAndDistanceQueryHandler(QueryChannelInterface queryChannel)
+        {
+            _queryChannel = queryChannel;
+        }
+
+        public List<Flier> Query(FindFlyersByLocationAndDistanceQuery argument)
+        {
+            var ret = _queryChannel.Query(argument, new List<string>());
+            return _queryChannel.Query(new FindByIdsQuery<Flier> {Ids = ret}, (List<Flier>)null);
+
+        }
     }
 }
