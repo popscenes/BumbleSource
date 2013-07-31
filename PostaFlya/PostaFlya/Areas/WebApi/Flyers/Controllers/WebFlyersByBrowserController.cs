4eb99049853afb413ff864932b3398c3de18552e
Anthony Borg <teddy@bumbleflya.com>
2013-07-31 21:52:41 +1000

    for merge again

---
+++ PostaFlya/PostaFlya/Areas/WebApi/Flyers/Controllers/WebFlyersByBrowserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Common.ApiInfrastructure.Controller;
+
+namespace PostaFlya.Areas.WebApi.Flyers.Controllers
+{
+    public class WebFlyersByBrowserController : WebApiControllerBase
+    {
+        //
+        // GET: /WebApi/WebFlyersByBrowser/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
