6e7f8839439684b5e061e4ac4c616af378e860d4
firinflames <rickyaudsley@gmail.com>
2013-08-01 21:11:46 +1000

    updates to sdk 2.1 some javascript updates as well

--- PostaFlya/PostaFlya/Scripts/_references.js
+++ PostaFlya/PostaFlya/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-ui-1.10.0.js" />
-/// <reference path="jquery-1.9.1.js" />
+﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="jquery-ui-1.10.0.js" />
 /// <reference path="jquery-1.8.1-vsdoc.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="knockout-2.1.0.debug.js" />
