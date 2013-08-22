bfcc955cc4ec8e2c53acf826db0148c3c94a88c5
Anthony Borg <teddy@bumbleflya.com>
2013-08-22 22:30:28 +1000

    inital test spent most of my time umming and ahhing

---
+++ Core/WebSite.Application.Azure.Tests/ServiceBus/AzureServicerBusTopicSubscriptionTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Website.Application.Azure.Tests.ServiceBus
+{
+    [TestFixture]
+    class AzureServicerBusTopicSubscriptionTests
+    {
+        public void TopicBusFactoryCreatesAzureMessageTopicBus()
+        {
+            var topicBusFactory = new AzureMessageTopicBusFactory();
+
+            var topicBus = topicBusFactory.GetTopicBus("Test");
+
+            //atm just check we return a TopicBus
+            Assert.IsTrue(topicBus != null);
+        }
+
+        public void SubscriptionFactoryCreatesAzureMessageSubscriptionProcesor()
+        {
+            var subscriptionFactory = new AzureMessageSubscriptionFactory();
+
+            var subscriptionProcessor = subscriptionFactory.GetSubscriptionProcessor("Testsucscription");
+
+            //atm just check we return a subscriptionprocessor
+            Assert.IsTrue(subscriptionProcessor != null);
+        }
+    }
+
+    public class AzureMessageSubscriptionFactory
+    {
+        public AzureMessageSubscriptionProcessor GetSubscriptionProcessor(string testsucscription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AzureMessageSubscriptionProcessor : SubscriptionProcessorInterface
+    {
+    }
+
+    public interface SubscriptionProcessorInterface
+    {
+    }
+
+    public class AzureMessageTopicBusFactory
+    {
+        public AzureMessageTopicBus GetTopicBus(string test)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AzureMessageTopicBus : TopicBusInterface
+    {
+    }
+
+    public interface TopicBusInterface
+    {
+    }
+}
